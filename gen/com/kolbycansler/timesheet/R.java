d15f56439417c03e0441f0aaaba1b9dfa24d8a70
ubundude <golfguy90@gmail.com>
2012-12-18 03:09:34 -0600

    Started work on the UI

--- gen/com/kolbycansler/timesheet/R.java
+++ gen/com/kolbycansler/timesheet/R.java
@@ -14,7 +14,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int addNewButton=0x7f070007;
+        public static final int editText1=0x7f070008;
+        public static final int menu_settings=0x7f07000b;
+        public static final int nextButton=0x7f070005;
+        public static final int previousButton=0x7f070004;
+        public static final int quickAddButton=0x7f070006;
+        public static final int tab1=0x7f070001;
+        public static final int tab2=0x7f070009;
+        public static final int tab3=0x7f07000a;
+        public static final int tabhost=0x7f070000;
+        public static final int totalDisplayextView=0x7f070003;
+        public static final int totalTextView=0x7f070002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -23,9 +34,18 @@ public final class R {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
+        public static final int addNew=0x7f040005;
         public static final int app_name=0x7f040000;
+        public static final int day=0x7f040009;
+        public static final int defaultTotal=0x7f040008;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int month=0x7f04000b;
+        public static final int notSet=0x7f040007;
+        public static final int quickAdd=0x7f040004;
+        public static final int report=0x7f040006;
+        public static final int total=0x7f040003;
+        public static final int week=0x7f04000a;
     }
     public static final class style {
         /** 
--- src/com/kolbycansler/timesheet/MainActivity.java
+++ src/com/kolbycansler/timesheet/MainActivity.java
@@ -3,6 +3,8 @@ package com.kolbycansler.timesheet;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,20 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TabHost tabHost = (TabHost)findViewById(R.id.tabhost);
+        tabHost.setup();
+        
+        TabSpec spec1 = tabHost.newTabSpec("Tab 1");
+        spec1.setContent(R.id.tab1);
+        spec1.setIndicator("Report");
+        
+        TabSpec spec2 = tabHost.newTabSpec("Tab 2");
+        spec2.setContent(R.id.tab2);
+        spec2.setIndicator("Day");
+        
+        TabSpec spec3 = tabHost.newTabSpec("Tab 3");
+        spec3.setContent(R.id.tab3);
+        spec3.setIndicator("Weeek");
     }
 
     @Override
